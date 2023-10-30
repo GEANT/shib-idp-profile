15b434eb6544cee0ff73571bc1ed10e5583d3fe6
jlauros <janne.lauros@csc.fi>
2023-10-30 12:19:44 +0200

    Replace 'net.shibboleth.utilities.java.support.logic.FunctionSupport' with 'net.shibboleth.shared.logic.FunctionSupport'

--- user-profile-impl/src/main/java/org/geant/shibboleth/plugin/userprofile/profile/config/UserProfileConfiguration.java
+++ user-profile-impl/src/main/java/org/geant/shibboleth/plugin/userprofile/profile/config/UserProfileConfiguration.java
@@ -42,7 +42,7 @@ import net.shibboleth.shared.annotation.constraint.NotEmpty;
 import net.shibboleth.shared.annotation.constraint.NotLive;
 import net.shibboleth.shared.annotation.constraint.Unmodifiable;
 import net.shibboleth.shared.logic.Constraint;
-import net.shibboleth.utilities.java.support.logic.FunctionSupport;
+import net.shibboleth.shared.logic.FunctionSupport;
 import net.shibboleth.shared.primitive.StringSupport;
 
 /**
